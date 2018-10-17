c9265a98cc37579be4723f60b3958d1de761884d
Christian V <viray.christianj@gmail.com>
2018-10-17 21:11:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_call)
   planeteer_call.collect {|planeteer_call| planeteer_call.capitalize + "!"}
 end 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words, assorted_words)
+  short_words
 end
 
 def find_the_cheese# code an argument here
