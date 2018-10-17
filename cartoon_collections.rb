0b4ab048341307c3e2aa1ff52a251de32a2bcd3a
Christian V <viray.christianj@gmail.com>
2018-10-17 21:39:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find {|cheese| cheese.include?(cheese)}
+  cheese.find? {|cheese| cheese.include?(cheese)}
 end
